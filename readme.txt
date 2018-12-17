2b828b33eedfa087d267486fa1d385cd9be791d4
liccilip <5@3243.ru>
2018-12-17 12:45:25 +0300

    fa

--- readme.txt
+++
@@ -1,3 +0,0 @@
-License
-
-Licensed under the Creative Commons Attribution 3.0 license. You can use this template for free in both personal as well as commercial projects. 
--- scripts/options.js
+++
@@ -1,6 +0,0 @@
-var vblog={
-        login:'liccilip',
-		replieLink:'10-luchshikh-veb-saitov-chtoby-naiti-besplatnye-odnorazovye-nomera-telefonov-p-s-komu-len-perevodit',
-		followTag:'ru--cmc',
-
-};
