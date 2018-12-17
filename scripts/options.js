177531078643d6829e694cc53f4ec74acbe5eef5
liccilip <5@3243.ru>
2018-12-17 04:48:31 +0300

    Heivi buk

---
+++ scripts/options.js
@@ -0,0 +1,6 @@
+var vblog={
+        login:'iccilip',
+		replieLink:'https://golos.io/ru--sms/@liccilip/10-luchshikh-veb-saitov-chtoby-naiti-besplatnye-odnorazovye-nomera-telefonov-p-s-komu-len-perevodit',
+		followTag:'ru--futbol',
+
+};	
