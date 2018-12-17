d21ea2694c6b43b440cc8d86d753747e4f44782b
liccilip <5@3243.ru>
2018-12-17 06:54:51 +0300

    iz tetle

--- scripts/options.js
+++ scripts/options.js
@@ -1,6 +1,6 @@
 var vblog={
         login:'iccilip',
-		replieLink:'https://golos.io/ru--sms/@liccilip/10-luchshikh-veb-saitov-chtoby-naiti-besplatnye-odnorazovye-nomera-telefonov-p-s-komu-len-perevodit',
-		followTag:'ru--futbol',
+		replieLink:'10-luchshikh-veb-saitov-chtoby-naiti-besplatnye-odnorazovye-nomera-telefonov-p-s-komu-len-perevodit',
+		followTag:'cmc',
 
 };	
